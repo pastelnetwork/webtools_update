8c17358ef2d6796ceecffc2da08672ee8e5dbf4a
akobrin1 <andrey.kobrin@gmail.com>
2023-01-05 16:00:08 -0500

    [PSL-521] Use PyNaCl library for webtools updater

---
+++ generate_keys.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Copyright (c) 2022 The Pastel Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+python3 remote_code_updater.py --newkey
+
